ab0a407b535c42a8790f0125edbc8c9bdc4b5bc4
faisal-nstu <faisal.amin@bs-23.com>
2023-03-06 19:53:33 +0600

    suppressed serializableCheck

--- src/store.js
+++ src/store.js
@@ -1,28 +1,3 @@
-// import thunk from "redux-thunk";
-// import promise from "redux-promise-middleware";
-// import { applyMiddleware, compose } from "redux";
-// import { configureStore } from "@reduxjs/toolkit";
-// import reducers from "./reducers";
-
-// const middlewares = [promise, thunk];
-
-// let composeEnhancers = compose;
-
-// if (process.env.NODE_ENV === "development") {
-//     const { logger } = require("redux-logger");
-
-//     middlewares.push(logger);
-
-//     if (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) {
-//         composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
-//     }
-// }
-
-// export default composeEnhancers(applyMiddleware(...middlewares))(configureStore)(reducers);
-
-
-
-
 // weird gotcha - install history v4.10.1
 // see open issue: https://github.com/supasate/connected-react-router/issues/312#issuecomment-647082777
 // import { createBrowserHistory, History } from "history";
@@ -38,7 +13,9 @@ const middlewares = [promise];
 const preloadedState = {};
 const store = configureStore({
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(promise),
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }).concat(middlewares),
   reducer: rootReducer(history),
   preloadedState
 });
