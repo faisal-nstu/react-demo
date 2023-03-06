264867b6b5e01f575d1abc476bc3b169ee6f9efd
faisal-nstu <faisal.amin@bs-23.com>
2023-03-06 23:10:57 +0600

    tweak ux

--- src/modules/dashboard/dashboard.component.js
+++ src/modules/dashboard/dashboard.component.js
@@ -12,7 +12,7 @@ const Dashboard = () => {
 
     const pokemonList = useSelector(state => state.dashboard.pokemonList);
     const [apiUrl, setApiUrl] = useState('https://pokeapi.co/api/v2/pokemon');
-    const [details, setsetDetails] = useState(null);
+    const [details, setDetails] = useState(null);
 
     useEffect(() => {
         dispatch(getPokemons(apiUrl));
@@ -20,51 +20,55 @@ const Dashboard = () => {
 
     const loadNextPage = () => {
         setApiUrl(pokemonList.next);
+        setDetails(null);
     };
 
     const loadPreviousPage = () => {
         setApiUrl(pokemonList.previous);
+        setDetails(null);
     };
 
     const loadDetails = (pokemon) => {
         axios.get(pokemon.url)
             .then((res) => {
-                setsetDetails(res.data);
+                setDetails(res.data);
             });
     };
 
     return (
-        <div>
+        <div id="dashboard">
             <h4>Pokemons</h4>
             <Container>
                 <Row>
                     <Col xs={7} className="px-0">
                         {
                             pokemonList && pokemonList.results && pokemonList.results.map((pokemon, idx) => (
-                                <div key={idx} className="list-item px-2 py-1">
-                                    <span className="cursor-pointer text-capitalize" onClick={() => loadDetails(pokemon)}>{pokemon.name}</span>
+                                <div key={idx} className="list-item px-2 py-1 cursor-pointer" onClick={() => loadDetails(pokemon)}>
+                                    <span className="text-capitalize">{pokemon.name}</span>
                                 </div>
                             ))
                         }
                     </Col>
-                    <Col className="details-panel py-2">
-                        {details && (
-                            <>
-                                <img src={details.sprites.front_shiny}/>
-                                <img src={details.sprites.back_shiny}/>
-                                <div className="text-capitalize">Name: {details.name}</div>
-                                <div>Height: {details.height}</div>
-                                <div>Weight: {details.weight}</div>
-                                <div>Abilities: {details.abilities.map((ability, aIdx) => <span key={aIdx} className="badge bg-secondary mx-1">{ability.ability.name}</span>)}</div>
-                            </>
-                        )}
+                    <Col className="details-panel py-2 text-primary">
+                        {
+                            details ? (
+                                <>
+                                    <img src={details.sprites.front_shiny} />
+                                    <img src={details.sprites.back_shiny} />
+                                    <div className="text-capitalize fs-5 fw-bold">{details.name}</div>
+                                    <div>Height: {details.height}</div>
+                                    <div>Weight: {details.weight}</div>
+                                    <div>Abilities: {details.abilities.map((ability, aIdx) => <span key={aIdx} className="badge bg-secondary mx-1">{ability.ability.name}</span>)}</div>
+                                </>
+                            ) : <span>Click on the rows from the list to see details</span>
+                        }
                     </Col>
                 </Row>
             </Container>
 
             <div className="mt-2 mb-2">
                 <button className="btn btn-secondary" onClick={loadPreviousPage} disabled={!pokemonList.previous}>Previous</button>
-                <button className="btn btn-secondary mx-2" onClick={loadNextPage}>Next</button>
+                <button className="btn btn-secondary mx-2" onClick={loadNextPage} disabled={!pokemonList.next}>Next</button>
             </div>
         </div>
     )
